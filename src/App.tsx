8831846c3ea29adbe9c37a8494869c4597af02fb
kenji saito <32677139+saikeen@users.noreply.github.com>
2022-01-09 09:37:39 +0900

    ChakraUIをインストールして、アプリケーションに反映

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
+import { Box, Center, Flex, Square, Text } from '@chakra-ui/react';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Flex className="header" backgroundColor="lightblue">
+        <Box>
+          <Text>BirdHealthcare</Text>
+        </Box>
+      </Flex>
+      <Flex className="main" backgroundColor="white">
+        <Box>
+          <Text>BirdHealthcare</Text>
+        </Box>
+      </Flex>
+      <Flex className="footer" backgroundColor="lightblue">
+        <Box>
+          <Text>footer</Text>
+        </Box>
+      </Flex>
+    </>
   );
-}
+};
 
 export default App;
